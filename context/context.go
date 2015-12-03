41704fc5496a308361830b8d88953de85349de80
Donal Byrne <donalb@pagero.com>
2015-12-03 16:56:20 +0100

    Added context struct.

--- context/context.go
+++ context/context.go
@@ -1,7 +1,16 @@
 package context
 import "time"
 
+type RequestType string
+
+const (
+	Publish RequestType = "PUB"
+	Request RequestType = "REQ"
+)
 
 type Context struct {
+	PutAppName string
+	PutTime time.Time
+	PutType RequestType
 	Timeout time.Time
 }
