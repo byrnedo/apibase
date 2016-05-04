e84e113ed3c4fb1fe9ae7805707a1847aac8f078
Donal Byrne <byrnedo@tcd.ie>
2016-05-04 20:40:25 +0200

    Helper to create pointers from variable. For use with protobuf really.

---
+++ helpers/pointerhelp/pointerhelp.go
@@ -0,0 +1,50 @@
+package pointerhelp
+
+
+func NewString(in string) *string {
+	return &in
+}
+
+func NewBool(in bool) *bool {
+	return &in
+}
+
+func NewInt(in int) *int {
+	return &in
+}
+
+func NewInt8(in int8) *int8 {
+	return &in
+}
+
+func NewInt16(in int16) *int16 {
+	return &in
+}
+
+func NewInt32(in int32) *int32 {
+	return &in
+}
+
+func NewInt64(in int64) *int64 {
+	return &in
+}
+
+func NewUint(in uint) *uint {
+	return &in
+}
+
+func NewUint8(in uint8) *uint8 {
+	return &in
+}
+
+func NewUint16(in uint16) *uint16 {
+	return &in
+}
+
+func NewUint32(in uint32) *uint32 {
+	return &in
+}
+
+func NewUint64(in uint64) *uint64 {
+	return &in
+}
