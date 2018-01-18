cd83b955315696bd795aad8a0df0f5b779961b3e
Donal Byrne <byrnedo@tcd.ie>
2018-01-18 09:15:48 +0100

    Added simple time helpers

---
+++ natsio/protobuf/helpers.go
@@ -0,0 +1,43 @@
+package protobuf
+
+import "time"
+
+func (m *NatsContext_Trail) SetDeadlineTs(ts time.Time) {
+	s := ts.Unix()
+	n := int32(ts.Nanosecond())
+
+	m.Deadline = &s
+	m.DeadlineNanos = &n
+}
+
+func (m *NatsContext_Trail) GetDeadlineTs() *time.Time {
+	if m.Deadline == nil {
+		return nil
+	}
+
+	dS := m.GetDeadline()
+	dN := m.GetDeadlineNanos()
+
+	t := time.Unix(dS, int64(dN))
+	return &t
+}
+
+func (m *NatsContext_Trail) SetTimeTs(ts time.Time) {
+	s := ts.Unix()
+	n := int32(ts.Nanosecond())
+
+	m.Time = &s
+	m.TimeNanos = &n
+}
+
+func (m *NatsContext_Trail) GetTimeTs() *time.Time {
+	if m.Time == nil {
+		return nil
+	}
+
+	dS := m.GetTime()
+	dN := m.GetTimeNanos()
+
+	t := time.Unix(dS, int64(dN))
+	return &t
+}
